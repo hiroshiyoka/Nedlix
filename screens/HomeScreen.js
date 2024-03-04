5ac41b346e88622aabc14621ad9f2a27382409b1
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-03-04 12:40:38 +0700

    feat: add theme style also add logo and icon for HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className="mb-3">
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="28" strokeWidth={2} color="white" />
+          <Text className="text-white text-xl font-bold">
+            <Text style={styles.text}>Ned</Text>lix
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="28" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
