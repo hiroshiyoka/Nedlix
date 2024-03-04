bc6347c0e5c036f28de04ed87f77c193148148d8
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-03-04 13:11:22 +0700

    feat: add handleClick method with useNavigation state

--- components/TrendingMovies.js
+++ components/TrendingMovies.js
@@ -1,16 +1,24 @@
 import { View, Text, TouchableWithoutFeedback, Dimensions } from "react-native";
 import React from "react";
 import Carousel from "react-native-snap-carousel";
+import { useNavigation } from "@react-navigation/native";
 
 var { width, height } = Dimensions.get("window");
 
 export default function TrendingMovies() {
+  const navigation = useNavigation();
+  const handleClick = () => {
+    navigation.navigate("Movie", item);
+  };
+
   return (
     <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
       <Carousel
         data={data}
-        renderItem={({ item }) => <MovieCard item={item} />}
+        renderItem={({ item }) => (
+          <MovieCard item={item} handleClick={handleClick} />
+        )}
         firstItem={1}
         inactiveSlideOpacity={0.6}
         sliderWidth={width}
@@ -21,9 +29,9 @@ export default function TrendingMovies() {
   );
 }
 
-const MovieCard = ({ item }) => {
+const MovieCard = ({ item, handleClick }) => {
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={handleClick}>
       <Image
         style={{
           width: width * 0.6,
