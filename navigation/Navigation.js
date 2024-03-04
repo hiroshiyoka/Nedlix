89052a73fe90a1785b54a72d2b0df099487284b9
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-03-04 12:17:10 +0700

    feat: create HomeScreen component and import to Navigation

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,11 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
