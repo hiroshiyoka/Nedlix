7035d3639e108bad91af875702d3783a6135fea2
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-11 02:55:17 +0700

    feat: add navigation and favorite functionality

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -6,15 +6,21 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
+import { styles } from "../theme";
+import { HeartIcon } from "react-native-heroicons/solid";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS === "ios";
 const verticalMargin = ios ? "" : "my-3";
 
 export default function PersonScreen() {
+  const navigation = useNavigation();
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
     <ScrollView
       className="flex-1 bg-neutral-900"
@@ -34,10 +40,7 @@ export default function PersonScreen() {
           <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
         </TouchableOpacity>
         <TouchableOpacity>
-          <HeartIcon
-            size="35"
-            color={isFavourite ? theme.background : "white"}
-          />
+          <HeartIcon size="35" color={isFavorite ? "red" : "white"} />
         </TouchableOpacity>
       </SafeAreaView>
     </ScrollView>
