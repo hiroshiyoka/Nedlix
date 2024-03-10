37bb83552306d151671abf8fed1c5276ce9ef3e1
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-11 02:52:24 +0700

    feat: add navigation header

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -1,14 +1,45 @@
-import { View, Text, Dimensions, Platform, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS === "ios";
+const verticalMargin = ios ? "" : "my-3";
 
 export default function PersonScreen() {
   return (
     <ScrollView
       className="flex-1 bg-neutral-900"
       contentContainerStyle={{ paddingBottom: 20 }}
-    ></ScrollView>
+    >
+      <SafeAreaView
+        className={
+          "z-20 w-full flex-row justify-between items-center px-4" +
+          verticalMargin
+        }
+      >
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.background}
+          className="rounded-xl p-1"
+        >
+          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <HeartIcon
+            size="35"
+            color={isFavourite ? theme.background : "white"}
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
