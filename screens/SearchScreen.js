544f09404500d9eca07c6b6637c713b9f4deb033
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-13 19:37:23 +0700

    feat: add TextInput and SafeAreaView from react-native

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import { View, Text, Dimensions } from "react-native";
+import { View, Text, Dimensions, TextInput } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const { width, height } = Dimensions.get("window");
 
 export default function SearchScreen() {
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView className="bg-neutral-800 flex-1">
+      <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
+        <TextInput
+          placeholder="Search Movie"
+          placeholderTextColor={"lightgray"}
+          className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
