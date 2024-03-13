63947ab03e032a09bf3b0b2be563ab742890f681
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-14 01:28:11 +0700

    feat: add loading indicator to PersonScreen

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -14,6 +14,7 @@ import { useNavigation } from "@react-navigation/native";
 import { styles } from "../theme";
 import { HeartIcon } from "react-native-heroicons/solid";
 import MovieList from "../components/MovieList";
+import Loading from "../components/Loading";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS === "ios";
@@ -23,6 +24,7 @@ export default function PersonScreen() {
   const navigation = useNavigation();
   const [isFavorite, setIsFavorite] = useState(false);
   const [personMovies, setPersonMovies] = useState([1, 2, 3, 4]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <ScrollView
@@ -47,66 +49,70 @@ export default function PersonScreen() {
         </TouchableOpacity>
       </SafeAreaView>
 
-      <View>
-        <View
-          className="flex-row justify-center"
-          style={{
-            shadowColor: "gray",
-            shadowRadius: 40,
-            shadowOffset: { width: 0, height: 5 },
-            shadowOpacity: 1,
-          }}
-        >
-          <View className="items-center rounded-full overflow-hidden h-72 w-72 border border-neutral-600">
-            <Image
-              source={require("../assets/images/michale.jpg")}
-              style={{ height: height * 0.43, width: width * 0.74 }}
-            />
+      {loading ? (
+        <Loading />
+      ) : (
+        <View>
+          <View
+            className="flex-row justify-center"
+            style={{
+              shadowColor: "gray",
+              shadowRadius: 40,
+              shadowOffset: { width: 0, height: 5 },
+              shadowOpacity: 1,
+            }}
+          >
+            <View className="items-center rounded-full overflow-hidden h-72 w-72 border border-neutral-600">
+              <Image
+                source={require("../assets/images/michale.jpg")}
+                style={{ height: height * 0.43, width: width * 0.74 }}
+              />
+            </View>
           </View>
-        </View>
-        <View className="mt-6">
-          <Text className="text-3xl text-white font-bold text-center">
-            Al Pacino
-          </Text>
-          <Text className="text-base text-neutral-500 text-center">
-            New York City, United States
-          </Text>
-        </View>
-        <View className="mx-3 mt-6 flex-row justify-between items-center bg-neutral-700 rounded-full">
-          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
-            <Text className="text-white font-semibold">Gender</Text>
-            <Text className="text-neutral-300 text-sm">Male</Text>
+          <View className="mt-6">
+            <Text className="text-3xl text-white font-bold text-center">
+              Al Pacino
+            </Text>
+            <Text className="text-base text-neutral-500 text-center">
+              New York City, United States
+            </Text>
           </View>
-          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
-            <Text className="text-white font-semibold">Birthday</Text>
-            <Text className="text-neutral-300 text-sm">1940-04-25</Text>
+          <View className="mx-3 mt-6 flex-row justify-between items-center bg-neutral-700 rounded-full">
+            <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+              <Text className="text-white font-semibold">Gender</Text>
+              <Text className="text-neutral-300 text-sm">Male</Text>
+            </View>
+            <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+              <Text className="text-white font-semibold">Birthday</Text>
+              <Text className="text-neutral-300 text-sm">1940-04-25</Text>
+            </View>
+            <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+              <Text className="text-white font-semibold">Known for</Text>
+              <Text className="text-neutral-300 text-sm">Acting</Text>
+            </View>
+            <View className="px-2 items-center">
+              <Text className="text-white font-semibold">Popularity</Text>
+              <Text className="text-neutral-300 text-sm">74</Text>
+            </View>
           </View>
-          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
-            <Text className="text-white font-semibold">Known for</Text>
-            <Text className="text-neutral-300 text-sm">Acting</Text>
+          <View className="my-6 mx-4 space-y-2">
+            <Text className="text-white text-lg">Biography</Text>
+            <Text className="text-neutral-400 tracking-wide">
+              Alfredo James Pacino (born April 25, 1940) is an American actor.
+              Considered one of the greatest and most influential actors of the
+              20th century, Pacino has received numerous accolades: including an
+              Academy Award, two Tony Awards, and two Primetime Emmy Awards
+              achieving the Triple Crown of Acting. He also received four Golden
+              Globe Awards, a BAFTA, two Screen Actors Guild Awards and been
+              honored with the Cecil B. DeMille Award in 2001, the AFI Life
+              Achievement Award in 2007, the National Medal of Arts in 2011, and
+              the Kennedy Center Honors in 2016.
+            </Text>
           </View>
-          <View className="px-2 items-center">
-            <Text className="text-white font-semibold">Popularity</Text>
-            <Text className="text-neutral-300 text-sm">74</Text>
-          </View>
-        </View>
-        <View className="my-6 mx-4 space-y-2">
-          <Text className="text-white text-lg">Biography</Text>
-          <Text className="text-neutral-400 tracking-wide">
-            Alfredo James Pacino (born April 25, 1940) is an American actor.
-            Considered one of the greatest and most influential actors of the
-            20th century, Pacino has received numerous accolades: including an
-            Academy Award, two Tony Awards, and two Primetime Emmy Awards
-            achieving the Triple Crown of Acting. He also received four Golden
-            Globe Awards, a BAFTA, two Screen Actors Guild Awards and been
-            honored with the Cecil B. DeMille Award in 2001, the AFI Life
-            Achievement Award in 2007, the National Medal of Arts in 2011, and
-            the Kennedy Center Honors in 2016.
-          </Text>
-        </View>
 
-        <MovieList title={"Movies"} hideSeeAll={true} data={personMovies} />
-      </View>
+          <MovieList title={"Movies"} hideSeeAll={true} data={personMovies} />
+        </View>
+      )}
     </ScrollView>
   );
 }
