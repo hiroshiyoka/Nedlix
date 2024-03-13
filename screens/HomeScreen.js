c3f636ece2176b794c1ac32c1e2d21025ebc4b20
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-14 01:21:21 +0700

    feat: add loading indicator

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,11 +10,13 @@ import { styles } from "../theme";
 import TrendingMovies from "../components/TrendingMovies";
 import MovieList from "../components/MovieList";
 import { useNavigation } from "@react-navigation/native";
+import Loading from "../components/Loading";
 
 export default function HomeScreen() {
   const [trending, setTrending] = useState([1, 2, 3]);
   const [upcoming, setUpcoming] = useState([1, 2, 3]);
   const [topRated, setTopRated] = useState([1, 2, 3]);
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
   return (
@@ -32,14 +34,18 @@ export default function HomeScreen() {
         </View>
       </SafeAreaView>
 
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 10 }}
-      >
-        <TrendingMovies data={trending} />
-        <MovieList title="Upcoming" data={upcoming} />
-        <MovieList title="Top Rated" data={topRated} />
-      </ScrollView>
+      {loading ? (
+        <Loading />
+      ) : (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 10 }}
+        >
+          <TrendingMovies data={trending} />
+          <MovieList title="Upcoming" data={upcoming} />
+          <MovieList title="Top Rated" data={topRated} />
+        </ScrollView>
+      )}
     </View>
   );
 }
