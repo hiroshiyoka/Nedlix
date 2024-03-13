60bb309cfc79d81a42034212b288171c118b64e9
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-13 19:33:45 +0700

    feat: add Dimensions from react-native

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import React from "react";
+
+const { width, height } = Dimensions.get("window");
 
 export default function SearchScreen() {
   return (
