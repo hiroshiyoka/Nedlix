bb1dfab35ab8b23af2530301ca9c3be5b38492ce
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-13 19:43:24 +0700

    feat: add navigation to Home Screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -8,10 +8,13 @@ import {
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XMarkIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
 
 const { width, height } = Dimensions.get("window");
 
 export default function SearchScreen() {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
       <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
@@ -21,7 +24,7 @@ export default function SearchScreen() {
           className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
         />
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={() => navigation.navigate("Home")}
           className="rounded-full p-3 m-1 bg-neutral-500"
         >
           <XMarkIcon size="25" color="white" />
