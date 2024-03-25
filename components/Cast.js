69923a0fbc75d655b45653b0f941c92a6b2c8c2b
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-26 01:22:56 +0700

    feat(cast): add fallback for person image in Cast component

--- components/Cast.js
+++ components/Cast.js
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import React from "react";
-import { image185 } from "../api/moviedb";
+import { fallbackPersonImage, image185 } from "../api/moviedb";
 
 export default function Cast({ cast, navigation }) {
   let personName = "Al Pacino";
@@ -25,7 +25,10 @@ export default function Cast({ cast, navigation }) {
                 <View className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
                   <Image
                     className="rounded-2xl h-24 w-20"
-                    source={{ uri: image185(person?.profile_path) }}
+                    source={{
+                      uri:
+                        image185(person?.profile_path) || fallbackPersonImage,
+                    }}
                   />
                 </View>
                 <Text className="text-white text-xs mt-1">
