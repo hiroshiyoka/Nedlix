7dd844087b4a7d9ac591e3a0503f59996be10a31
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-26 01:21:01 +0700

    feat(api): add fallback URLs for movie poster and person image

--- api/moviedb.js
+++ api/moviedb.js
@@ -22,6 +22,11 @@ export const image342 = (path) =>
 export const image185 = (path) =>
   path ? `https://image.tmdb.org/t/p/w185${path}` : null;
 
+export const fallbackMoviePoster =
+  "https://img.myloview.com/stickers/white-laptop-screen-with-hd-video-technology-icon-isolated-on-grey-background-abstract-circle-random-dots-vector=illustration-400-176057922.jpg";
+export const fallbackPersonImage =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmUiF-YGjavA63_Au8jQj7zxnFxS_Ay9xc6pxleMqCxH92SzeNSjBTwZ0l61E4B3KTS70&usqp=CAU";
+
 const apiCall = async (endpoint, params) => {
   const options = {
     method: "GET",
